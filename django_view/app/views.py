f7dbca2b22dce8cba075b86bcbcd52be1049ab7e
hilmisarioglu <77903324+hilmisarioglu@users.noreply.github.com>
2021-10-13 21:24:10 +0200

    v1

---
+++ django_view/app/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+def home(request):
+    # print(request.GET.get('q'))
+    # print(request.COOKIES)
+    # print(request.path)
+    # print(request.user)
+    # print(request.method)
+    
+    # if request.method == 'GET':
+    #     print('You are using GET method!')
+   
+    # if request.method == 'GET':
+    #     print(f'You are using {request.method} method!')
+    
+    # print(request.META)
+    
+    context = {
+       'title' : 'clarusway',
+       'my_list':[2,3,4] 
+    }
+    return render(request, 'app/home.html', context)
