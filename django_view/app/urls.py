b66717e4ae40ff8c61ebf65f00ecf3fa48001822
hilmisarioglu <77903324+hilmisarioglu@users.noreply.github.com>
2021-12-09 20:26:00 +0100

    tags

--- django_view/app/urls.py
+++ django_view/app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home , contact
 
 urlpatterns = [
     path('', home, name='home'),
+    path('contact/', contact, name='contact'),
 ]
--- django_view/app/views.py
+++ django_view/app/views.py
@@ -4,23 +4,26 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    # print(request.GET.get('q'))
-    # print(request.COOKIES)
-    # print(request.path)
-    # print(request.user)
-    # print(request.method)
-    
-    # if request.method == 'GET':
-    #     print('You are using GET method!')
+   # print(request.path)
+   # print(request.GET.get('q'))
+   # print(request.COOKIES)
+   # print(request.user) #burda hata almamak icin login olup admin sayfas olustur
+   # print(request.method) 
    
-    # if request.method == 'GET':
-    #     print(f'You are using {request.method} method!')
+   # if request.method == 'GET':
+   #     print('You are using GET method!')
+   
+   # if request.method == 'GET':
+   #    print(f'You are using {request.method} method!')
     
-    # print(request.META)
+   # print(request.META) # Demin tek tek cektik simdi hepsini cekiyoruz
     
-    context = {
-       'title' : 'clarusway',
-       'dict1' : {'title' : 'Hilmi'},
-       'my_list': [2,3,4] 
-    }
-    return render(request, 'app/home.html', context)
+   context = {
+      'title' : 'clarusway',
+      'dict1' : {'title' : 'Hilmi'},
+      'my_list': [1,2,3,4] 
+   }
+   return render(request, 'app/home.html', context)
+
+def contact(request):
+   return render(request, 'app/contact.html')
